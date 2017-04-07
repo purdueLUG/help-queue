1b915f87b9bd0846dcde7b5a7082046b8327b946
Jack Cottom <iacobus96@gmail.com>
2017-04-07 11:50:27 -0400

    eliminate redundant GET requests

--- public/javascripts/client_queue.js
+++ public/javascripts/client_queue.js
@@ -3,13 +3,22 @@ var socket = io();
 // asynchronous receive behavior
 socket.on('enqueue', function(msg){
     // regex for finding course
-    var re = ".*/(.*)$";
-    // first element is full path, second is matched in the parens
-    var course = window.location.pathname.match(re)[1];
+    var re = "/(.*)/(.*)$";
+    // first element is full path, second is student or ta, third is course
+    var match = window.location.pathname.match(re);
+    var view = match[1];
+    var course = match[2];
 
     // only enqueue if on the right course
     if (course == msg.course){
-        $('body').load(window.location.href);
+        //$('body').load(window.location.href);
+        if(view == 'student'){
+            $('#queueList').append('<li class=\'list-group-item\'>' + msg.name + '</li>');
+        }
+        else if (view == 'ta'){
+            //$('#queueList').append('<tr><td>' + msg.name + '</td><td>test</td></tr>');
+            $('#queueList').append('<tr><td>' + msg.name + '</td><td>' + Date().toLocaleString() + '</td></tr>');
+        }
     }
 });
 
@@ -21,6 +30,7 @@ socket.on('dequeue', function(msg){
 
     // only dequeue if on the right course
     if (course == msg.course){
-        $('body').load(window.location.href);
+        //$('body').load(window.location.href);
+        $('#queueList :first-child').remove();
     }
 });
